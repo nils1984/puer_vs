58a03fba25a253b38b14cd2f0afe9cfa557eb7fc
Administrator <Administrator@sx-lsy>
2017-08-30 11:20:48 +0800

    1

---
+++ cli_sevcall/src/main/java/com/puer/sevcall/vsclient/ComputeClient.java
@@ -0,0 +1,14 @@
+package com.puer.sevcall.vsclient;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+@FeignClient("compute-service")
+public interface ComputeClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+}
