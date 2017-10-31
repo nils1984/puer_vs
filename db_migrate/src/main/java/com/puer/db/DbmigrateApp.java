5b502c39de6c1d57dc36e7a59c77eaa8ce293282
Administrator <Administrator@sx-lsy>
2017-10-31 10:29:58 +0800

    1

---
+++ db_migrate/src/main/java/com/puer/db/DbmigrateApp.java
@@ -0,0 +1,18 @@
+package com.puer.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DbmigrateApp 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(DbmigrateApp.class, args);
+    }
+
+}
